b6bf1e8cf57b939d49766277247e719f9bfeced7
SOWMYA SRI <150457690+SOWMYASRI7@users.noreply.github.com>
2024-08-22 12:56:15 +0530

    modified mongodb replica

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 //const cors=require('cors')
 const studentRouter = require('./routes/students')
 
-
 //const url = 'mongodb://127.0.0.1:27017/data'
 const url ='mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
 const app = express()
