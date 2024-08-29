42d7bc33b4c450295174457387b8b1f709212eb8
SOWMYA SRI <150457690+SOWMYASRI7@users.noreply.github.com>
2024-08-29 12:13:51 +0530

    Updated mongodb Atlas url in app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 //const cors=require('cors')
-const studentRouter = require('./routes/students')
+const studentRouter = require('./routes/controller')
 
 //const url = 'mongodb://127.0.0.1:27017/data'
-const url ='mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+//const url ='mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+const url ='mongodb+srv://sowmyanainala7:<password>@mern-crud.8uwkr.mongodb.net/?retryWrites=true&w=majority&appName=mern-crud';
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
